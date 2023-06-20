392288e8b394d8f40f18f89b19a6c6596d36f1c1
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-06-20 17:06:32 -0400

    holders for prophet and demon

--- R/demon.R
+++ R/demon.R
@@ -1,28 +1,7 @@
-## .................................................................................
-## Purpose: Demon variatonal autoencoder to thwart prophet
-##
-## Author: Nick Brazeau
-##
-## Date: 22 February, 2023
-##
-## Notes:
-## VAE built heavily from https://github.com/ageron/handson-ml2
-## tutorial on AE in R https://nbisweden.github.io/workshop-neural-nets-and-deep-learning/session_rAutoencoders/lab_autoencoder_hapmap.html
-## https://towardsdatascience.com/intuitively-understanding-variational-autoencoders-1bfe67eb5daf
-## .................................................................................
-#install.packages("tensorflow")
-#install.packages("keras")
-library(tidyverse)
-library(reticulate)
-library(tensorflow)
-library(keras)
-reticulate::use_python("/Users/nbrazeau/Documents/Github/prophetdemon/venv/bin/python")
-reticulate::use_virtualenv("/Users/nbrazeau/Documents/Github/prophetdemon/venv")
+#' @title Coding Sampler for Demon VAE
+#' @noMd
+#' @noRd
 
-
-#++++++++++++++++++++++++++++++++++++++++++
-###  functions     ####
-#++++++++++++++++++++++++++++++++++++++++++
 CodingSampler(keras::keras$layers$Layer) %py_class% {
   call <- function(inputs) {
     c(codings_mean, codings_log_var) %<-% inputs
@@ -37,16 +16,14 @@ CodingSampler(keras::keras$layers$Layer) %py_class% {
 
 
 
-#++++++++++++++++++++++++++++++++++++++++++
-### VAE Demon Model Setup        ####
-#++++++++++++++++++++++++++++++++++++++++++
-#............................................................
-# Set up demon ENcoder
-#...........................................................
+#' @title Encoder for Demon VAE
+#' @noMd
+#' @noRd
+
 Encoder(keras$layers$Layer) %py_class% {
   "Maps CNN to a triplet (z_mean, z_log_var, z)."
 
-  initialize <- function(latent_dim = 32, intermediate_dim = 64,
+  initialize <- function(latent_dim, intermediate_dim,
                          name = "encoder", ...) {
     super$initialize(name = name, ...)
     self$codings_proj <- layer_dense(units = intermediate_dim,
@@ -65,18 +42,15 @@ Encoder(keras$layers$Layer) %py_class% {
   }
 }
 
+#' @title Decoder for Demon VAE
+#' @noMd
+#' @noRd
 
-
-
-
-#............................................................
-# Set up demon DEcoder
-#...........................................................
 Decoder(keras$layers$Layer) %py_class% {
   "Converts z-codings, the encoded digit vector, back into a adjacency matrix."
 
   initialize <- function(original_dim,
-                         intermediate_dim = 64,
+                         intermediate_dim,
                          name = "decoder", ...) {
     super$initialize(name = name, ...)
     self$codings_proj <- keras::layer_dense(units = intermediate_dim,
@@ -91,32 +65,48 @@ Decoder(keras$layers$Layer) %py_class% {
   }
 }
 
+#' @title VAE: Combined Encoder-Decoder for full Demon
+#' @param
+#' @description
+#' @details
+#' @returns
+#' @noMd
+#' @noRd
 
-#............................................................
-# Set up demon combined EN-DEcoder VAE
-#...........................................................
 VariationalAutoEncoder(keras$Model) %py_class% {
-  "Combines the encoder and decoder into an end-to-end VAE model for training."
-  initialize <- function(original_dim, intermediate_dim = 64, latent_dim = 32,
-                         name = "autoencoder", ...) {
-    super$initialize(name = name, ...)
-    self$original_dim <- original_dim
+  classname = "VAE: Combines the encoder and decoder into an end-to-end model"
+  public = list(encoder = NULL,
+                decoder = NULL,
+                original_dim = NULL,
+                intermediate_dim = NULL,
+                latent_dim = NULL,
 
-    self$encoder <- Encoder(
-      latent_dim = latent_dim,
-      intermediate_dim = intermediate_dim
-    )
-    self$decoder <- Decoder(
-      original_dim,
-      intermediate_dim = intermediate_dim
-    )
-  }
+                initialize <- function(original_dim, intermediate_dim, latent_dim,
+                                       name = "autoencoder", ...) {
+                  super$initialize(name = name, ...)
+                  self$original_dim <- original_dim
+
+                  self$encoder <- Encoder(
+                    latent_dim = latent_dim,
+                    intermediate_dim = intermediate_dim
+                  )
+                  self$decoder <- Decoder(
+                    original_dim,
+                    intermediate_dim = intermediate_dim
+                  )
+                  # fill in dims for public
+                  self$original_dim <- original_dim
+                  self$intermediate_dim <- intermediate_dim
+                  self$latent_dim <- latent_dim
+
+                }
+  )
 
   call <- function(inputs) {
     c(codings_mean, codings_log_var, codings) %<-% self$encoder(inputs)
     reconstructed <- self$decoder(codings)
     # Add KL divergence regularization loss.
-    kl_loss <- -0.5 * keras::k_sum(1 + codings_log_var - keras::k_exp(codings_log_var) - keras::k_square(codings_mean), axis=-1)
+    kl_loss <- -0.5 * tf$reduce_mean(codings_log_var - tf$square(codings_mean) - tf$exp(codings_log_var) + 1)
     self$add_loss(kl_loss)
     reconstructed
   }
@@ -124,49 +114,4 @@ VariationalAutoEncoder(keras$Model) %py_class% {
 
 
 
-#............................................................
-# Set up VAE demon optimizer and learning rates
-#...........................................................
-N <- 10 # numn inidivid..uals
-original_dim <- N^2
-vae <- VariationalAutoEncoder(original_dim, 64, 32)
-
-
-#++++++++++++++++++++++++++++++++++++++++++
-### Training VAE Demon        ####
-#++++++++++++++++++++++++++++++++++++++++++
-#..............................
-# generate training set
-#...............................
-train_size <- 1e1
-rewireprob <- runif(train_size, min = 0.1, max = 0.5)
-grphs <- sapply(rewireprob, function(x, n){
-  out <- igraph::erdos.renyi.game(n = n,
-                                  p.or.m = x,
-                                  type = "gnp")
-  out <- igraph::as_adjacency_matrix(out, sparse = F)
-  return(out)
-}, n = N, simplify = "array")
-
-# rearrange
-x_train <- grphs %>%
-  array_reshape(c(train_size, N^2))
-
-
-#......................
-# train demon model
-#......................
-demon <- VariationalAutoEncoder(N^2, 64, 32)
-optimizer <- optimizer_adam(learning_rate = 1e-3)
-demon %>% compile(optimizer, loss = loss_mean_squared_error())
-demon %>% fit(x_train, x_train, epochs = 2, batch_size = 2)
-
-
-#......................
-# predict from demon model
-#......................
-inputnets <- rbinom(n = 100, size = 1, prob = 0.05)
-inputnets <- t(inputnets)
-# new nets
-new_nets <- demon$predict(inputnets)
 
--- R/prophet.R
+++ R/prophet.R
@@ -1,5 +1,54 @@
+# https://tensorflow.rstudio.com/guides/keras/making_new_layers_and_models_via_subclassing.html#putting-it-all-together-an-end-to-end-example
+# https://tensorflow.rstudio.com/guides/keras/customizing_what_happens_in_fit.html#wrapping-up-an-end-to-end-gan-example
+# https://tensorflow.rstudio.com/guides/keras/training_with_built_in_methods
+# https://tensorflow.rstudio.com/guides/keras/making_new_layers_and_models_via_subclassing
+
+#++++++++++++++++++++++++++++++++++++++++++
+### Prophet        ####
+#++++++++++++++++++++++++++++++++++++++++++
+#' @title
+#' @param
+#' @description
+#' @details
+#' @returns
+#' @export
+
+prophet_model <- new_model_class(
+  # name of model class
+  classname = "prophet-sequentialNN",
+
+  # Initialize Method for Prophet Sequential Class
+  initialize = function(sequential_NNmodel) { # users will input layers manually
+    # catch
+    if (!any(class(sequential_NNmodel) %in% "keras.engine.sequential.Sequential") ) {
+      stop("Model must be created with keras::keras_model_sequential")
+    }
+    super$initialize()
+    self$sequential_NNmodel <- sequential_NNmodel
+  },
+
+  # Define compilation method
+  compile = function(optimizer, loss_fn) {
+    super$compile()
+    self$optimizer <- optimizer
+    self$loss_fn <- loss_fn
+  },
+
+  # define fit method
+  fit = function(fit) {
+    super$fit()
+    self$fit <- fit
+  }
+
+  # define evaluation method
+
+  # define predict method
+
+)
+
+
 ## .................................................................................
-## Purpose: Using tensor_flow and reticulate to build the prophet_bi
+## Purpose: Using tensor_flow and reticulate to build the prophet
 ##
 ## Author: Nick Brazeau & David Rasmussen
 ##
@@ -59,7 +108,7 @@ true_final_sizes <- tensorflow::array_reshape(true_final_sizes, dim = c(nrow(tra
 
 
 #............................................................
-# Set up prophet_bi model
+# Set up prophet model
 #   For Conv layers: first argument is the # of filters, second argument is the kernel size
 #   To think about: Should we sort rows/columns in adjacency matrix so most well-connected nodes are closer
 #   Maybe by single-linkage clustering: https://www.section.io/engineering-education/hierarchical-clustering-in-python/
@@ -68,7 +117,7 @@ n <- 1e2 # population size, needs to be same from sim
 batch_size <- 10
 input_shape_dim <- c(batch_size, n, n, 1)
 # make CNN model
-prophet_bi <- keras::keras_model_sequential() %>%
+prophet <- keras::keras_model_sequential() %>%
   keras::layer_conv_2d(., filters = 2, kernel_size = 8, activation = "relu", padding = "same", input_shape = input_shape_dim[2:length(input_shape_dim)]) %>%
   keras::layer_max_pooling_2d(pool_size = 2) %>%
   keras::layer_conv_2d(., filters = 4, kernel_size = 4, activation = "relu", padding = "same") %>%
@@ -81,7 +130,7 @@ prophet_bi <- keras::keras_model_sequential() %>%
   keras::layer_dense(., units = 1, activation = "sigmoid")
 
 # see summary of what we made
-summary(prophet_bi)
+summary(prophet)
 
 
 
@@ -93,7 +142,7 @@ summary(prophet_bi)
 
 #true_final_sizes <- tensorflow::array_reshape(true_final_sizes, dim = c(batch_size, 1))
 
-prophet_bi %>% compile(
+prophet %>% compile(
   optimizer = keras::optimizer_adam(),  # Optimizer
   # Loss function to minimize
   loss = keras::loss_mean_squared_error(),
@@ -101,7 +150,7 @@ prophet_bi %>% compile(
   metrics = list(keras::metric_mean_absolute_error())
 )
 
-history <- prophet_bi %>%
+history <- prophet %>%
   keras::fit(
     train_dat_net,
     true_final_sizes,
